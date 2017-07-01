6ba2fd2eaba4604016a139a249c5b6e642c8e32e
Kevin <contact@wuuker.com>
2017-07-01 20:29:14 +1000

    button testing
    button testing

---
+++ form/form_button bkup.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>Button CSS</title>
+<link rel="stylesheet" href="css/button2.css">
+</head>
+
+<body>
+
+<a href="#" class="button big green">sign in <span>One minute</span></a>
+ 
+<a href="#" class="button big blue">sign in <span>One minute</span></a>
+ 
+<a href="#" class="button big gray">sign in <span>One minute</span></a>
+
+
+<!--
+<a href="#" class="button big green">button</a>
+<a href="#" class="button big blue">button</a>
+<a href="#" class="button big gray">button</a>
+-->
+
+
+</body>
+</html>
--- form/form_button.php
+++ form/form_button.php
@@ -1,16 +1,17 @@
 <html>
 <head>
 <title>Button CSS</title>
-<link rel="stylesheet" href="css/button2.css">
+<link rel="stylesheet" href="css/button.css">
+<meta http-equiv="refresh" content="5" >
 </head>
 
 <body>
 
-<a href="#" class="button big green">sign in <span>One minute</span></a>
- 
-<a href="#" class="button big blue">sign in <span>One minute</span></a>
- 
-<a href="#" class="button big gray">sign in <span>One minute</span></a>
+<a href="#" class="profile_button big orangebox">Wuuker<span>Manage your wuuks</span></a>
+<br>
+<a href="#" class="profile_button big greenbox">Deliverer<span>Manage your delivery</span></a>
+<br>
+<a href="#" class="profile_button big brownbox">History<span>View all your History</span></a>
 
 
 <!--
@@ -19,6 +20,6 @@
 <a href="#" class="button big gray">button</a>
 -->
 
-
+<footer>Updated: 01 July 2017</footer>
 </body>
 </html>
